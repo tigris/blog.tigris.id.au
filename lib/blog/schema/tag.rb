398a45a20377f8d0ebe026ff66b7dff974e798a4
Danial Pearce <git@tigris.id.au>
2011-01-05 11:13:39 +1100

    Fix splat issue that was stopping me from sleeping at night

--- lib/blog/schema/tag.rb
+++ lib/blog/schema/tag.rb
@@ -5,7 +5,7 @@ module Blog
       attribute :post_id, Swift::Type::Integer
       attribute :name,    Swift::Type::String
 
-      def self.destroy(post_id, names)
+      def self.destroy(post_id, *names)
         placeholder = (['?'] * names.size).join(',')
         Blog.db.execute("delete from :tags where :post_id = ? and :name in (#{placeholder})", post_id, names)
       end
