e6aaec313a01bcf05403c3e72f82f4edc56c0313
Danial Pearce <git@tigris.id.au>
2011-01-05 16:31:51 +1100

    Invoke create/destroy on the Swift::Adapter directly for multi insert/delete goodness

--- lib/blog/post.rb
+++ lib/blog/post.rb
@@ -13,14 +13,11 @@ module Blog
         params = params.merge(slug: generate_slug(params[:title]))
         tags = params.delete('tags').split rescue []
         post = nil
-        Blog.db.transaction do
-          post = Schema::Post.create(params).first
-          # TODO: find out why swift isn't accepting the following array to create()
-          # tags = Schema::Tag.create(tags.map{|t| {post_id: post.id, name: t} })
-
-          # Also TODO: find out why this generates missing foreign key error,
-          # surely the create() above on Scheme::Post generates said key?
-          tags.map!{|t| Schema::Tag.create(post_id: post.id, name: t) }
+        Blog.db do |db|
+          db.transaction do
+            post = Schema::Post.create(params).first
+            db.create Schema::Tag, *tags.map{|t| {post_id: post.id, name: t} }
+          end
         end
         post
       end
@@ -29,13 +26,15 @@ module Blog
         post = search(identifier)
         params = params.dup
         tags   = params.delete('tags').split rescue []
-        Blog.db.transaction do
-          post = Schema::Post.update(params)
-          current_tags = Schema::Tag.all(':post_id = ?', post.id)
-          # TODO: the below passing of array is known to not work yet, see create() method above.
-          Schema::Tag.create((tags - current_tags).map{|t| {post_id: post.id, name: t} })
-          Schema::Tag.destroy(post.id, current_tags - tags)
+        Blog.db do |db|
+          db.transaction do
+            post = Schema::Post.update(params)
+            current_tags = Schema::Tag.all(':post_id = ?', post.id)
+            db.create Schema::Tag, *(tags - current_tags).map{|t| {post_id: post.id, name: t} }
+            db.destroy Schema::Tag, *(current_tags - tags).map{|t| {post_id: post.id, name: t} }
+          end
         end
+        post
       end
 
       private
--- lib/blog/schema/tag.rb
+++ lib/blog/schema/tag.rb
@@ -6,11 +6,6 @@ module Blog
       store :tags
       attribute :post_id, Swift::Type::Integer
       attribute :name,    Swift::Type::String
-
-      def self.destroy(post_id, *names)
-        placeholder = (['?'] * names.size).join(',')
-        Blog.db.execute("delete from :tags where :post_id = ? and :name in (#{placeholder})", post_id, names)
-      end
     end
   end
 end
