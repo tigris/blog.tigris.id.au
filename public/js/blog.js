d76be793799726f9244c545de82a5e8e06a2a8c5
Danial Pearce <git@tigris.id.au>
2011-01-09 21:58:05 +1100

    Make the tabs work

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,3 +1,15 @@
+// TODO: some form of class implementation so each page has it's own class with
+// init() function that would set the tab.
+//
+// See: http://ajaxian.com/archives/jquery-class
+
 $(function() {
-  $('#home').attr({ 'class': 'selected' });
+  var url = document.location.pathname;
+  var doc;
+  if (url.match(/^\/?$/))
+    $('#home').addClass('selected');
+  else if (url.match(/^\/posts/))
+    $('#posts').addClass('selected');
+  else if (url.match(/^\/tags/))
+    $('#posts').addClass('selected');
 });
